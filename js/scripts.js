98a0c3d00dcca63af841181577e333387b64312e
Stewart ilondanga <stilondanga@gmail.com>
2019-10-09 16:28:05 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,14 @@ var nextButton = document.querySelector('.next__step-button');
 var previousButton = document.querySelector('.previous__step-button');
 var stepTwo = document.querySelector('.signup__step-2');
 
-/*var slideIn = function() {
+var slideIn = function() {
   nextButton.addEventListener('click', function(event) {
     stepTwo.classList.remove('slide-out');
     stepTwo.classList.add('slide-in');
   });
 }
 
-var slideOut = function() {
+/*var slideOut = function() {
   previousButton.addEventListener('click', function(event) {
     stepTwo.classList.remove('slide-in');
     stepTwo.classList.add('slide-out');
