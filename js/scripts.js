fbe4a066f256ce76ed7bd5bc42ecf3c0fb9ccb44
Stewart ilondanga <stilondanga@gmail.com>
2019-10-09 16:25:36 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
-var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+/*var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+var nextButton = document.querySelector('.next__step-button');
+var previousButton = document.querySelector('.previous__step-button');
+var stepTwo = document.querySelector('.signup__step-2');
+
+var slideIn = function() {
+  nextButton.addEventListener('click', function(event) {
+    stepTwo.classList.remove('slide-out');
+    stepTwo.classList.add('slide-in');
+  });
+}
+
+var slideOut = function() {
+  previousButton.addEventListener('click', function(event) {
+    stepTwo.classList.remove('slide-in');
+    stepTwo.classList.add('slide-out');
+  });
+}
+
+if (nextButton && previousButton) {
+  slideIn();
+  slideOut();
+}
